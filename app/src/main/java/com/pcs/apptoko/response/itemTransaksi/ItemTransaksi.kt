678c3089d0eb4b2b7f579c478c9c3489dfe00fbb
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2022-12-27 17:25:03 +0700

    update code pertemuan 12

---
+++ app/src/main/java/com/pcs/apptoko/response/itemTransaksi/ItemTransaksi.kt
@@ -0,0 +1,10 @@
+package com.pcs.apptoko.response.itemTransaksi
+
+class ItemTransaksi (
+        val harga_saat_transaski: String,
+        val id: String,
+        val produk_id: String,
+        val qty: String,
+        val sub_total: String,
+        val transaksi_id: String
+        )
